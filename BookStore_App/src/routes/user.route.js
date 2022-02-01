12a928759c21daf6f46cdaa6055c9ae681120aa1
mandeep <singh.mr.1996@gmail.com>
2022-02-01 10:07:18 +0530

    [Mandeep] Refacter: refacter req.body validation for registration

--- BookStore_App/src/routes/user.route.js
+++ BookStore_App/src/routes/user.route.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
 import { setRole } from '../utils/user.util';
 
 const router = express.Router();
 
 //route to create a new user registration
-router.post('/user', setRole('user'), newUserValidator, userController.register);
+router.post('/user', setRole('user'), userController.register);
 
 //route to create a new admin registration
-router.post('/admin', setRole('admin'), newUserValidator, userController.register);
+router.post('/admin', setRole('admin'), userController.register);
 
 export default router;
